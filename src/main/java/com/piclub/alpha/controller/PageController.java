37dc40c6987af49fafc724f40b47af58e08d75aa
pqiu <massiveqp@aliyun.com>
2018-12-08 22:11:59 +0800

    Keep rest APIs; Use MVC & templates to control pages for now

---
+++ src/main/java/com/piclub/alpha/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.piclub.alpha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/activity/create")
+    public String getBlankActivity() {
+        return "createAct";
+    }
+
+    @GetMapping("/activity/{actId}/edit")
+    public String getFilledActivity() {
+        return "modifyAct";
+    }
+}
--- src/main/resources/static/js/piclub.js
+++ src/main/resources/static/js/piclub.js
@@ -2,7 +2,7 @@ angular.module("piclubApp", []).controller("MainCtrl", ['$http', function($http)
 	var me = this;
 	
 	me.items = [];
-	$http.get('/activities').then(function(response) {
+	$http.get('./activities').then(function(response) {
 		me.items = response.data;
 	}, function(errResponse) {
 		console.error('Error while fetch activities');
@@ -15,13 +15,17 @@ angular.module("piclubApp", []).controller("MainCtrl", ['$http', function($http)
 
 	};
 
-	me.submit = function(){
-		console.log('Hello moto');
+	me.createAct = function(){
+		console.log('create act...');
 		//submit
 	};
 
 	me.upload = function(){
 		console.log('uploading');
-	}
+	};
+
+	me.modifyAct = function () {
+      console.log('modify act...')
+    };
 
 }]);
