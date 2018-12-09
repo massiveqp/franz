ee69976253ff2f1610ed2322b7b5b00359aed644
pqiu <massiveqp@aliyun.com>
2018-12-09 21:02:04 +0800

    Add place picker input; Add datetime picker's js&css

--- src/main/resources/static/js/piclub.js
+++ src/main/resources/static/js/piclub.js
@@ -1,9 +1,10 @@
 angular.module("piclubApp", []).controller("MainCtrl", ['$http', function($http){
 	var me = this;
-	
-	me.items = [];
+	var act = {};
+
+	me.acts = [];
 	$http.get('./activities').then(function(response) {
-		me.items = response.data;
+		me.acts = response.data;
 	}, function(errResponse) {
 		console.error('Error while fetch activities');
 	});
@@ -15,16 +16,24 @@ angular.module("piclubApp", []).controller("MainCtrl", ['$http', function($http)
 
 	};
 
+	me.places = [
+        {label: '漾学堂', id: 1},
+        {label: '猎学堂', id: 2},
+        {label: '常乐书房', id: 3}
+    ];
+	me.selectedPlaceId = 1;
+
 	me.createAct = function(){
 		console.log('create act...');
-		//submit
+		me.act.place = me.selectedPlaceId;
+		console.log(me.act);
 	};
 
 	me.upload = function(){
 		console.log('uploading');
 	};
 
-	me.modifyAct = function () {
+	me.modifyAct = function() {
       console.log('modify act...')
     };
 
